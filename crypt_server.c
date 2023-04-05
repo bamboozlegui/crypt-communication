629b7f1054c5e30825a457b71496d9778e6b05ae
bamboozlegui <kartoffelgutgut@gmail.com>
2023-04-06 02:42:42 +0300

    validate socket creation

--- crypt_server.c
+++ crypt_server.c
@@ -2,17 +2,19 @@
 
 Author: me
 Description: Navigate in the maze and don't get caught
+
+If compiling on Windows link -lws2_32
 */
 
 
 #ifdef _WIN32
 #include <winsock2.h>
-#pragma comment(lib,"ws2_32.lib") // need this - Winsock library
 #else
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 #endif
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,12 +25,12 @@ int main(int argc, char* argv[])
 {
 #ifdef _WIN32
     WSADATA wsa;
-#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
-#define GETSOCKETERRNO() (WSAGetLastError())
+#define IS_VALID_SOCKET(s) ((s) != INVALID_SOCKET)
+#define GET_SOCKET_ERR() (WSAGetLastError())
 #else
-#define    SOCKET int // Winsock uses SOCKET object
-    ISVALIDSOCKET(s) ((s) >= 0)
-    GETSOCKETERRNO() (errno)
+#define SOCKET int
+#define IS_VALID_SOCKET(s) ((s) >= 0)
+#define GET_SOCKET_ERR() (errno)
 #endif
 
     SOCKET listen_socket;
@@ -40,12 +42,15 @@ int main(int argc, char* argv[])
 
     int c_address_length;
 
-    printf("Validating port...");
+    printf("Validating port...\n");
     if (argc != 2)
     {
-        printf("Missing arguments: %s <port>");
+        printf("Missing arguments: %s <port>", argv[0]);
         exit(-1);
     }
+    printf("Port validated.\n");
+
+    port = atoi(argv[1]);
 
     if (port < 1 || port > 65535)
     {
@@ -54,19 +59,22 @@ int main(int argc, char* argv[])
     }
 
 #ifdef _WIN32
-    printf("Initialising Winsock...");
+    printf("Initialising Winsock... \n");
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
-        printf("Unsuccessful. Error: $d", WSAGetLastError());
+        printf("Unsuccessful. Error: %d\n", WSAGetLastError());
         exit(-1);
     }
-    printf("Successful.");
+    printf("Successful.\n");
 #endif
 
     // create server socket
-    if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0))) // SOCK_STREAM - connection oriented TCP protocol
+    printf("Creating socket...\n");
+    listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(!IS_VALID_SOCKET(listen_socket))
     {
-        ;
+        fprintf(stderr, "Failed in socket creation: %d\n", GET_SOCKET_ERR());
     }
+    printf("Successful.\n");
     return 1;
 }

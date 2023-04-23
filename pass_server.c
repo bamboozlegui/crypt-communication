2bac20ed771ce3c142ec6eb75688945af84378d4
bamboozlegui <44553051+bamboozlegui@users.noreply.github.com>
2023-04-23 13:46:47 +0300

    Update pass_server.c

--- pass_server.c
+++ pass_server.c
@@ -1,12 +1,3 @@
-/*
-
-Author: me
-Description: Navigate in the maze and don't get caught
-
-If compiling on Windows link -lws2_32
-*/
-
-
 #ifdef _WIN32
 #include <winsock2.h>
 
@@ -25,20 +16,19 @@ If compiling on Windows link -lws2_32
 #define IS_VALID_SOCKET(s) ((s) >= 0)
 #define GET_SOCKET_ERR() (errno)
 #define CLOSE_SOCKET(s) close(s)
-
 #endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>   
 #include <time.h>
 
-#define KEY_LENGTH 9
-#define MAX_PASS_LENGTH 256
+#define MAX_COUNT 20
 #define BUFF_LENGTH 1024
 
 char* generate_pass(int n, char* pass);
-void parse_buffer(char* buffer, int* pass_length, char key[]);
+void parse_buffer(char* buffer, int* pass_length, int* pass_count);
 
 int main(int argc, char* argv[])
 {
@@ -60,31 +50,34 @@ int main(int argc, char* argv[])
 
     struct sockaddr_in server_address;
     struct sockaddr_in client_address;
-    char buffer[BUFF_LENGTH];
-    char key[9];
-    int pass_length;
-    char pass[256];
-    memset(key, 0, sizeof(key));
-    
     socklen_t client_addr_length = sizeof(struct sockaddr);
 
-    printf("Validating port...\n");
+
+    char buffer[BUFF_LENGTH];
+    int pass_count;
+    int pass_length;
+    char pass[256];
+    srand(time(NULL)); // seed for pseudo-random number generations
+
+
+    printf("Validating arguments...\n");
     if (argc != 2)
     {
         printf("Missing arguments: %s <port>\n", argv[0]);
         exit(-1);
     }
-    printf("Port validated!\n");
+    printf("Arguments passed!\n");
 
     port = atoi(argv[1]);
 
+    printf("Validating connection port...\n");
     if (port < 1 || port > 65535)
     {
         printf("Port number out of range. Allowed: [1,65535]");
         exit(-1);
     }
+    printf("Connection port validated!\n");
 
-    // create server socket
     printf("Creating socket...\n");
     listen_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (!IS_VALID_SOCKET(listen_socket))
@@ -95,15 +88,15 @@ int main(int argc, char* argv[])
 
     // clear socket memory and fill with addr struct
     memset(&server_address, 0, sizeof(server_address));
-    // set protocol
+
+    // set protocol, address(to all network interfaces), port
     server_address.sin_family = AF_INET;
-    // set address (all network interfaces)
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    // set port
     server_address.sin_port = htons(port);
 
-    printf("Binding to socket...\n"); 
-    if (bind(listen_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+    printf("Binding to socket...\n");
+    if (bind(listen_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0)
+    {
         fprintf(stderr, "Failed while binding socket. Error: %d", GET_SOCKET_ERR());
         exit(1);
     }
@@ -116,8 +109,8 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Failed while creating listen socket. Error: %d\n", GET_SOCKET_ERR());
     }
     printf("Start accepting...\n");
-    int r = 0;
-    for(;;)
+
+    for (;;)
     {
         // clear buffer and client address struct
         memset(&client_address, 0, sizeof(client_address));
@@ -125,19 +118,50 @@ int main(int argc, char* argv[])
 
         // wait for client to connect
         accept_socket = accept(listen_socket, (struct sockaddr*)&client_address, &client_addr_length);
-        if(!IS_VALID_SOCKET(accept_socket))
+        if (!IS_VALID_SOCKET(accept_socket))
         {
             fprintf(stderr, "Failed while accepting socket. Error: %s\n", strerror(GET_SOCKET_ERR()));
             exit(-1);
         }
-        
-        // get key
-        r = recv(accept_socket, buffer, sizeof(buffer), 0);
-        printf("Received buffer: %s\n", buffer);
+        printf("Peer connected.\n");
 
-        // send data to client
-        send(accept_socket, buffer, strlen(buffer), 0);
+        // #1 get password lengt and count from client
+        recv(accept_socket, buffer, sizeof(buffer), 0);
+        printf("Received password length and count: %s\n", buffer);
+
+        parse_buffer(buffer, &pass_length, &pass_count);
+        printf("Parsed length and count: %i and %i\n", pass_length, pass_count);
+
+        if (pass_length > 21 || pass_count > 21)
+        {
+            // #2 send info about password validity
+            printf("Password arguments are out of bound.\n");
+            strncpy(buffer, "Max value for password length or count exceeded. Length and count are set to 20.\n", 82);
+            send(accept_socket, buffer, strlen(buffer), 0);
+
+            pass_length = 20;
+            pass_count = 20;
+        }
+        else
+        {
+            strncpy(buffer, "Password arguments are valid.\n", 31);
+            send(accept_socket, buffer, 31, 0);
+        }
+
+        // #3 convert send password count
+        sprintf(buffer, "%i", pass_count);
+        send(accept_socket, buffer, 3, 0);
+
+        printf("Here are your passwords:\n");
+        for (int i = 0; i < pass_count; ++i)
+        {
+            // #4 send passwords
+            generate_pass(pass_length, pass);
+            printf("Generating pass #%i.... %s\n", i + 1, pass);
+            send(accept_socket, pass, pass_length, 0);
+        }
         CLOSE_SOCKET(accept_socket);
+        printf("Peer disconnected.\n");
     }
 
     return 1;
@@ -145,35 +169,21 @@ int main(int argc, char* argv[])
 
 char* generate_pass(int n, char* pass)
 {
-    
     char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};':\"\\|,.<>/?`~";
-    //char pass[n + 1];
-    srand(time(NULL)); // seed for pseudo-random number generator
-    
     for (int i = 0; i < n; ++i)
     {
-        pass[i] = chars[rand() % sizeof(chars) - 1]; // select random character
+        pass[i] = chars[rand() % (sizeof(chars) - 1)]; // select random character
     }
     pass[n] = '\0';
 
     return pass;
 }
 
-char* encrypt_message(char* message, int length)
-{
-    for (int i = 0; i < length; i++)
-    {
-        message[0] += 2;
-    }
-}
-
-void parse_buffer(char* buffer, int* pass_length, char key[])
+void parse_buffer(char* buffer, int* pass_length, int* pass_count)
 {
     char* token = strtok(buffer, " ");
 
     *pass_length = atoi(token);
     token = strtok(NULL, " ");
-    key = token;
-    printf("%s", key);
-    token = strtok(NULL, " ");
+    *pass_count = atoi(token);
 }
